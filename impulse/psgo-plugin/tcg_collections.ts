7144507c6fc22bbe354f8caeb98bfa12d680ace1
PrinceSky-Git <princeskygithub@gmail.com>
2025-10-04 15:03:49 +0530

    Update tcg_collections.ts

--- impulse/psgo-plugin/tcg_collections.ts
+++ impulse/psgo-plugin/tcg_collections.ts
@@ -3,9 +3,17 @@
  * Central definition of all MongoDB collections used by the TCG plugin.
  */
 
-import { MongoDB } from '../../impulse/mongodb_module';
-import { TCGCard, UserCollection } from './tcg_data';
+import { MongoDB } from '../../mongodb_module';
 
+// Existing collections
 export const TCGCards = MongoDB<TCGCard>('tcg_cards');
 export const UserCollections = MongoDB<UserCollection>('tcg_user_collections');
 export const ShopStateCollection = MongoDB<ShopState>('tcg_shop_state');
+
+// New ranking system collections
+export const PlayerRankings = MongoDB<PlayerRanking>('tcg_player_rankings');
+export const BattleHistories = MongoDB<BattleHistory>('tcg_battle_histories');
+export const RankingSeasons = MongoDB<RankingSeason>('tcg_ranking_seasons');
+export const DailyChallenges = MongoDB<DailyChallenge>('tcg_daily_challenges');
+export const SimulatedBattles = MongoDB<SimulatedBattle>('tcg_simulated_battles');
+export const SeasonRewards = MongoDB<SeasonReward>('tcg_season_rewards');
